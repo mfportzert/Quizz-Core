a3d57b89e46e2e94ffcae66bb06db783ae6caa00
mfportzert <michelf.portzert@gmail.com>
2012-12-29 02:03:10 +0100

    Added a Class to auto set at VISIBLE a View which has an animation starting

---
+++ src/com/quizz/core/listeners/VisibilityAnimatorListener.java
@@ -0,0 +1,31 @@
+package com.quizz.core.listeners;
+
+import java.lang.ref.WeakReference;
+
+import android.view.View;
+
+import com.actionbarsherlock.internal.nineoldandroids.animation.Animator;
+import com.actionbarsherlock.internal.nineoldandroids.animation.Animator.AnimatorListener;
+
+public class VisibilityAnimatorListener implements AnimatorListener {
+		
+	View mView;
+	
+	public VisibilityAnimatorListener(View view) {
+		mView = view;
+	}
+	
+	@Override
+	public void onAnimationStart(Animator animation) {
+		mView.setVisibility(View.VISIBLE);
+	}
+
+	@Override
+	public void onAnimationEnd(Animator animation) {}
+
+	@Override
+	public void onAnimationCancel(Animator animation) {}
+
+	@Override
+	public void onAnimationRepeat(Animator animation) {}
+}
