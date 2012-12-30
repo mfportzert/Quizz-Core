c4537559df3032d1c7f27c40605a3d39003db149
mfportzert <michelf.portzert@gmail.com>
2012-12-30 01:01:52 +0100

    Now implements FragmentContainer

--- src/com/quizz/core/activities/BaseQuizzActivity.java
+++ src/com/quizz/core/activities/BaseQuizzActivity.java
@@ -1,42 +1,70 @@
 package com.quizz.core.activities;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
 import android.view.Window;
+import android.view.animation.LinearInterpolator;
+import android.widget.HorizontalScrollView;
+import android.widget.ImageView;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.internal.nineoldandroids.animation.ObjectAnimator;
 import com.quizz.core.R;
+import com.quizz.core.interfaces.FragmentContainer;
 
-public class BaseQuizzActivity extends SherlockFragmentActivity {
+public class BaseQuizzActivity extends SherlockFragmentActivity implements FragmentContainer {
+	
+	private boolean mBackgroundImageAnimated = true;
+	private ImageView mBackgroundImage;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_quizz);
+		
+		mBackgroundImage = (ImageView) findViewById(R.id.backgroundAnimatedImage);
+		
+		if (mBackgroundImageAnimated) {
+			
+			// FIXME: May not be displayed correctly on bigger screen when looping (bad transition)
+			// TODO: Make an image with beginning left similar to right end
+			// TODO: Scroll the horizontalScrollView instead of translating the
+			// imageView
+			HorizontalScrollView bgAnimatedImageContainer = (HorizontalScrollView) 
+					findViewById(R.id.backgroundAnimatedImageContainer);
+			bgAnimatedImageContainer.setOnTouchListener(new OnTouchListener() {
+	
+				@Override
+				public boolean onTouch(View v, MotionEvent event) {
+					return true;
+				}
+			});
+			
+			initActivityAnimations();
+		}
 	}
 	
-	protected void navigateTo(Class<?> cls) {
-		
-		FragmentManager fragmentManager = getSupportFragmentManager();
-		try {
-			Fragment fragment = fragmentManager.findFragmentByTag(cls.getSimpleName());
-            if (fragment == null) {
-            	fragment = (Fragment) cls.newInstance();
-            }
-            
-            if (!fragment.isAdded()) { // if fragment is already on the screen, do nothing
-            	FragmentTransaction transaction = fragmentManager.beginTransaction();
-            	transaction.replace(R.id.fragmentsContainer, (Fragment) cls.newInstance(), "test");
-            	transaction.commit();
-            }
-			
-		} catch (InstantiationException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		}
+	/**
+	 * Call this method before super.onCreate()<br />
+	 * Prevents a useless background animation if you don't want to set an image
+	 */
+	protected void requestNoAnimatedBackgroundImage() {
+		mBackgroundImageAnimated = false;
+	}
+	
+	private void initActivityAnimations() {
+		ObjectAnimator bgAnimation = ObjectAnimator.ofFloat(mBackgroundImage, "translationX", 500, -1700);
+		bgAnimation.setDuration(30000);
+		bgAnimation.setInterpolator(new LinearInterpolator());
+		bgAnimation.setRepeatCount(ObjectAnimator.INFINITE);
+		bgAnimation.start();
+	}
+
+	@Override
+	public int getId() {
+		return R.id.fragmentsContainer;
 	}
 }
