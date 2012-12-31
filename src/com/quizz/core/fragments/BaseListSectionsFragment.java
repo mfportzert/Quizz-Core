ac12fb0ae9bf14fca878c870b5f92a10140c03da
mfportzert <michelf.portzert@gmail.com>
2012-12-31 02:06:57 +0100

    Load sections from json using gson

--- src/com/quizz/core/fragments/BaseListSectionsFragment.java
+++ src/com/quizz/core/fragments/BaseListSectionsFragment.java
@@ -1,10 +1,18 @@
 package com.quizz.core.fragments;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Collection;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.quizz.core.models.Section;
 
 public abstract class BaseListSectionsFragment extends Fragment {
@@ -17,7 +25,7 @@ public abstract class BaseListSectionsFragment extends Fragment {
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
-        onSectionsLoaded(null);
+		loadSections();
 	}
 	
 	@Override
@@ -34,6 +42,21 @@ public abstract class BaseListSectionsFragment extends Fragment {
     public void onResume() {
         super.onResume();
     }
+	
+	private void loadSections() {
+		Gson gson = new Gson();
+		Type type = new TypeToken<Collection<Section>>(){}.getType();
+		InputStream is;
+		try {
+			is = getResources().getAssets().open("quizz.json");
+			Reader reader = new InputStreamReader(is);
+			ArrayList<Section> sections = gson.fromJson(reader, type);
+	        onSectionsLoaded(sections);
+		} catch (IOException e) {
+			// TODO: Make a reload button for errors
+			e.printStackTrace();
+		}
+	}
 	
 	protected abstract void onSectionsLoaded(ArrayList<Section> listSections);
 }
