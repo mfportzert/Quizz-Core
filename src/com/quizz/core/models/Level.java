98346c2728e54fbe1f84125ed92fa6949c74861b
mfportzert <michelf.portzert@gmail.com>
2012-12-31 02:06:39 +0100

    Made models parcelable

--- src/com/quizz/core/models/Level.java
+++ src/com/quizz/core/models/Level.java
@@ -1,11 +1,68 @@
 package com.quizz.core.models;
 
-public class Level {
+import com.google.gson.annotations.SerializedName;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Level implements Parcelable {
 	
+	public static final Parcelable.Creator<Level> CREATOR = new Parcelable.Creator<Level>() {
+		
+		public Level createFromParcel(Parcel parcel) {
+			return new Level(parcel);
+        }
+		
+        public Level[] newArray(int size) {
+        	return new Level[size];
+        }
+	};
+	
+	@SerializedName("hint1")
 	public String firstHint;
+	
+	@SerializedName("hint2")
 	public String secondHint;
+	
+	@SerializedName("responses")
 	public String response;
+	
+	@SerializedName("description")
+	public String description;
+	
+	@SerializedName("link")
 	public String moreInfosLink;
+	
+	@SerializedName("image")
 	public String imageName;
-	public int difficulty;
+	
+	@SerializedName("difficulty")
+	public String difficulty;
+	
+	/**
+	 * @param parcel
+	 */
+	public Level(Parcel parcel) {
+		this.firstHint = parcel.readString();
+		this.secondHint = parcel.readString();
+		this.response = parcel.readString();
+		this.description = parcel.readString();
+		this.imageName = parcel.readString();
+		this.difficulty = parcel.readString();
+	}
+	
+	@Override
+	public int describeContents() {
+		return 0;
+	}
+
+	@Override
+	public void writeToParcel(Parcel dest, int flags) {
+		dest.writeString(this.firstHint);
+		dest.writeString(this.secondHint);
+		dest.writeString(this.response);
+		dest.writeString(this.description);
+		dest.writeString(this.imageName);
+		dest.writeString(this.difficulty);
+	}
 }
--- src/com/quizz/core/models/Section.java
+++ src/com/quizz/core/models/Section.java
@@ -2,10 +2,53 @@ package com.quizz.core.models;
 
 import java.util.ArrayList;
 
-public class Section {
+import android.os.Parcel;
+import android.os.Parcelable;
 
-	public ArrayList<Level> levels = new ArrayList<Level>();
+import com.google.gson.annotations.SerializedName;
+
+public class Section implements Parcelable {
+	
+	public static final Parcelable.Creator<Section> CREATOR = new Parcelable.Creator<Section>() {
+		
+		public Section createFromParcel(Parcel parcel) {
+			return new Section(parcel);
+        }
+		
+        public Section[] newArray(int size) {
+        	return new Section[size];
+        }
+	};
+	
+	@SerializedName("section")
+	public int number;
+	
+	@SerializedName("name")
 	public String name;
-	public int levelsCount;
-	public int levelsComplete;
+	
+	@SerializedName("levels")
+	public ArrayList<Level> levels = new ArrayList<Level>();
+	
+	/**
+	 * @param parcel
+	 */
+	public Section(Parcel parcel) {
+		
+		this.number = parcel.readInt();
+		this.name = parcel.readString();
+		parcel.readList(this.levels, Section.class.getClassLoader());
+	}
+	
+	@Override
+	public int describeContents() {
+		return 0;
+	}
+
+	@Override
+	public void writeToParcel(Parcel dest, int flags) {
+		
+		dest.writeInt(this.number);
+		dest.writeString(this.name);
+		dest.writeList(this.levels);
+	}
 }
