d331bd9582c2ca679c15891adeff3dc4156a8584
mfportzert <michelf.portzert@gmail.com>
2013-01-19 18:03:22 +0100

    Added createFromAssets method in ImageUtils

---
+++ src/com/quizz/core/utils/ImageUtils.java
@@ -0,0 +1,18 @@
+package com.quizz.core.utils;
+
+import java.io.IOException;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+
+public class ImageUtils {
+
+	public static Drawable createFromAsset(Context context, String filePath) {
+		try {
+			return Drawable.createFromStream(context.getAssets().open(filePath), null);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}	
+}
