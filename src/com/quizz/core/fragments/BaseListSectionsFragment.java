db05208a5e669a233a972c6dd81ec587d5096c65
mfportzert <michelf.portzert@gmail.com>
2013-01-19 18:06:40 +0100

    Changed Arraylist to List

--- src/com/quizz/core/fragments/BaseListSectionsFragment.java
+++ src/com/quizz/core/fragments/BaseListSectionsFragment.java
@@ -7,6 +7,7 @@ import java.io.Reader;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -62,5 +63,5 @@ public abstract class BaseListSectionsFragment extends Fragment {
 		}
 	}
 	
-	protected abstract void onSectionsLoaded(ArrayList<Section> listSections);
+	protected abstract void onSectionsLoaded(List<Section> listSections);
 }
