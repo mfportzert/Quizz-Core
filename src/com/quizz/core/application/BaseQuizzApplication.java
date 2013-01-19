33ac8376543d889229f219311f41dcc8321d17f5
mfportzert <michelf.portzert@gmail.com>
2013-01-19 18:06:21 +0100

    Refactored Db access and usage

--- src/com/quizz/core/application/BaseQuizzApplication.java
+++ src/com/quizz/core/application/BaseQuizzApplication.java
@@ -1,45 +1,26 @@
 package com.quizz.core.application;
 
-import com.quizz.core.db.DbHelper;
 import android.app.Application;
-import android.content.Context;
-import android.database.sqlite.SQLiteDatabase;
-import android.util.Log;
+
+import com.quizz.core.db.DbHelper;
 
 public class BaseQuizzApplication extends Application {
 
     private DbHelper mDbHelper;
-    private static BaseQuizzApplication mSelf;
-
-    public synchronized static SQLiteDatabase db() {
-		if (self() == null)
-			Log.d("BaseQuizzApplication", "self() returned null");
-        if (self().mDbHelper == null)
-            self().mDbHelper = new DbHelper(self());
-        return self().mDbHelper.open();
-    }
-
-    public static Context context() {
-        return self();
-    }
-
-    private static BaseQuizzApplication self() {
-        return mSelf;
-    }
     
+    public synchronized DbHelper getDbHelper() {
+        return mDbHelper;
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
-        mSelf = this;
-        Log.d("BaseQuizzApplication", "onCreate");
+        mDbHelper = new DbHelper(this);
     }
     
     @Override
     public void onTerminate() {
     	super.onTerminate();
-      	if (self().mDbHelper != null && self().mDbHelper.isOpen())
-      		self().mDbHelper.close();
-      	self().mDbHelper = null;
-	}
-    
+    	mDbHelper.close();
+    }
 }
