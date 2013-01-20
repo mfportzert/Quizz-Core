37208e6a1595bc3ab375b6a3366fc9deb72d3016
mfportzert <michelf.portzert@gmail.com>
2013-01-20 21:51:23 +0100

    Fill adapter with real values

--- src/com/quizz/core/fragments/BaseGridLevelsFragment.java
+++ src/com/quizz/core/fragments/BaseGridLevelsFragment.java
@@ -1,65 +1,37 @@
 package com.quizz.core.fragments;
 
-import java.io.InputStream;
-import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.Collection;
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.widget.ArrayAdapter;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 import com.quizz.core.models.Level;
 import com.quizz.core.models.Section;
 
 public abstract class BaseGridLevelsFragment extends Fragment {
 
+    public static final String ARG_SECTION = "BaseGridLevelsFragment.ARG_SECTION";
+    
+    protected ArrayAdapter<Level> mAdapter;
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
 	super.onCreate(savedInstanceState);
+	Section section = getArguments().getParcelable(ARG_SECTION);
+	fillAdapter(section);
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
 	super.onActivityCreated(savedInstanceState);
-	loadLevels();
     }
-
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-	super.onSaveInstanceState(outState);
-    }
-
-    @Override
-    public void onPause() {
-	super.onPause();
-    }
-
-    @Override
-    public void onResume() {
-	super.onResume();
-    }
-
-    private void loadLevels() {
-	Gson gson = new Gson();
-	Type type = new TypeToken<Collection<Section>>() {
-	}.getType();
-	InputStream is;
-	/*
-	 * try { is = getResources().getAssets().open("quizz.json"); Reader
-	 * reader = new InputStreamReader(is); ArrayList<Level> levels =
-	 * gson.fromJson(reader, type); } catch (IOException e) { // TODO: Make
-	 * a reload button for errors e.printStackTrace(); }
-	 */
-	ArrayList<Level> levels = new ArrayList<Level>();
-	for (int i = 0; i < 30; i++) {
-	    Level level = new Level();
-	    level.imageName = "big_ben.jpg";
-	    levels.add(level);
+    
+    private void fillAdapter(Section section) {
+	mAdapter.clear();
+	for (Level level : section.levels) {
+	    mAdapter.add(level);
 	}
-	onLevelsLoaded(levels);
+	mAdapter.notifyDataSetChanged();
     }
 
-    protected abstract void onLevelsLoaded(ArrayList<Level> listLevels);
+    protected abstract void initAdapter(ArrayAdapter<Level> adapter);
 }
