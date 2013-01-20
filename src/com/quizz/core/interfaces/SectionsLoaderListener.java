8edeeb036a6d3503f712c69429c31cf68c83e3e2
mfportzert <michelf.portzert@gmail.com>
2013-01-20 21:50:13 +0100

    Added listener for sections loading

---
+++ src/com/quizz/core/interfaces/SectionsLoaderListener.java
@@ -0,0 +1,10 @@
+package com.quizz.core.interfaces;
+
+import java.util.List;
+
+import com.quizz.core.models.Section;
+
+public interface SectionsLoaderListener {
+    public void onSectionsLoading();
+    public void onSectionsLoaded(List<Section> listSections);
+}
