63ca7aa247ddfb77a293077bbf85749b156a8279
mfportzert <michelf.portzert@gmail.com>
2013-02-08 03:13:54 +0100

    Fix: ActionBar not showing when rotating screen

--- src/com/quizz/core/fragments/BaseStatsFragment.java
+++ src/com/quizz/core/fragments/BaseStatsFragment.java
@@ -3,31 +3,37 @@ package com.quizz.core.fragments;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 
+import com.quizz.core.activities.BaseQuizzActivity;
+
 public class BaseStatsFragment extends Fragment {
-	
-	@Override
+
+    @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	super.onCreate(savedInstanceState);
     }
-	
-	@Override
-	public void onActivityCreated(Bundle savedInstanceState) {
-		super.onActivityCreated(savedInstanceState);
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+	if (getActivity() instanceof BaseQuizzActivity) {
+	    ((BaseQuizzActivity) getActivity())
+		    .setHideAbOnRotationChange(false);
 	}
-	
-	@Override
-	public void onSaveInstanceState(Bundle outState) {
-		super.onSaveInstanceState(outState);
-	}
-	
-	@Override
+	super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+	super.onSaveInstanceState(outState);
+    }
+
+    @Override
     public void onPause() {
-        super.onPause();
+	super.onPause();
     }
-	
-	@Override
+
+    @Override
     public void onResume() {
-        super.onResume();
+	super.onResume();
     }
 
 }
