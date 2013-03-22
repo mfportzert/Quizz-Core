ed1e1c7a19c1706e1a5a3a01e02d17a15bb0d2f7
mfportzert <michelf.portzert@gmail.com>
2013-03-23 00:32:30 +0100

    NavigationUtils: Added a forceReplace parameter on navigation methods, in order to allow adding in backstack a new Fragment of the same class as the currently displayed one

--- src/com/quizz/core/utils/NavigationUtils.java
+++ src/com/quizz/core/utils/NavigationUtils.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 
 import com.actionbarsherlock.internal.nineoldandroids.animation.Animator;
 import com.actionbarsherlock.internal.nineoldandroids.animation.Animator.AnimatorListener;
@@ -11,7 +12,8 @@ import com.actionbarsherlock.internal.nineoldandroids.animation.AnimatorSet;
 import com.quizz.core.interfaces.FragmentContainer;
 
 public class NavigationUtils {
-
+	private static final String TAG = NavigationUtils.class.getSimpleName();
+	
 	public static void animatedNavigationTo(final Class<?> cls,
 			final FragmentManager fragmentManager,
 			final FragmentContainer container, final boolean addToStack,
@@ -42,40 +44,51 @@ public class NavigationUtils {
 		}
 	}
 
-	public static void directNavigationTo(Class<?> cls,
-			FragmentManager fragmentManager, FragmentContainer container,
-			boolean addToStack, FragmentTransaction transaction) {
-		directNavigationTo(cls, fragmentManager, container, addToStack,
-				transaction, null);
+	public static void directNavigationTo(Class<?> cls, FragmentManager fragmentManager, 
+			FragmentContainer container, boolean addToStack, FragmentTransaction transaction) {
+		directNavigationTo(cls, fragmentManager, container, addToStack, transaction, null, false);
 	}
 
+	public static void directNavigationTo(Class<?> cls, FragmentManager fragmentManager, 
+			FragmentContainer container, boolean addToStack, FragmentTransaction transaction, 
+			Bundle args) {
+		directNavigationTo(cls, fragmentManager, container, addToStack, transaction, args, false);
+	}
+	
+	/**
+	 * @param cls
+	 * @param fragmentManager
+	 * @param container
+	 * @param addToStack
+	 * @param forceReplace - if the fragment is already added but must be refreshed (new instance of same fragment)
+	 * @param transaction
+	 * @param args
+	 */
 	public static void directNavigationTo(Class<?> cls,
 			FragmentManager fragmentManager, FragmentContainer container,
-			boolean addToStack, FragmentTransaction transaction, Bundle args) {
+			boolean addToStack, FragmentTransaction transaction, Bundle args, boolean forceReplace) {
 		try {
-			Fragment fragment = fragmentManager.findFragmentByTag(cls
-					.getSimpleName());
-			if (fragment == null) {
+			Fragment fragment = fragmentManager.findFragmentByTag(cls.getSimpleName());
+			if (fragment == null || forceReplace) {
 				fragment = (Fragment) cls.newInstance();
 			}
 
 			// if fragment is already on the screen, do nothing
-			if (!fragment.isAdded()) {
+			if (!fragment.isAdded() || forceReplace) {
 				if (addToStack) {
 					transaction.addToBackStack(null);
 				}
 				if (args != null) {
 					fragment.setArguments(args);
 				}
-				transaction.replace(container.getId(), fragment,
-						cls.getSimpleName());
+				transaction.replace(container.getId(), fragment, cls.getSimpleName());
 				transaction.commit();
 			}
 
 		} catch (InstantiationException e) {
-			e.printStackTrace();
+			Log.e(TAG, e.getMessage(), e);
 		} catch (IllegalAccessException e) {
-			e.printStackTrace();
+			Log.e(TAG, e.getMessage(), e);
 		}
 	}
 }
