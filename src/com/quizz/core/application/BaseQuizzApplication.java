4838bae649cda287c45130c809447266a8b3b228
mfportzert <michelf.portzert@gmail.com>
2013-05-08 18:14:34 +0200

    Moved Preferences constants to PreferencesUtils class

--- src/com/quizz/core/application/BaseQuizzApplication.java
+++ src/com/quizz/core/application/BaseQuizzApplication.java
@@ -9,19 +9,6 @@ public class BaseQuizzApplication extends Application {
 
 	public static final String HIDE_AB_ON_ROTATION_CHANGE = 
 			"BaseQuizzApplication.HIDE_AB_ON_ROTATION_CHANGE";
-	public static final String PREF_VERSION_KEY = 
-			"BaseQuizzApplication.VERSION";
-	public static final String PREF_UNLOCKED_HINTS_COUNT_KEY = 
-			"BaseQuizzApplication.UNLOCKED_HINTS_COUNT";
-	public static final String PREF_DEFAULT_NB_HINTS_ONSUCCESS_KEY = 
-			"BaseQuizzApplication.DEFAULT_NB_HINTS_ONSUCCESS";
-	public static final String PREF_AUDIO_KEY = "BaseQuizzApplication.AUDIO";
-	public static final String PREF_VIBRATION_KEY = "BaseQuizzApplication.VIBRATION";
-	public static final String PREF_EXIT_POPUP_KEY = "BaseQuizzApplication.EXIT_POPUP";
-	
-	public static final int PREF_VERSION_VALUE = 1;
-	public static final int PREF_DEFAULT_UNLOCKED_HINTS_COUNT_VALUE = 3;
-	public static final int PREF_DEFAULT_NB_HINTS_ONSUCCESS_VALUE = 2;
 	
 	@Override
 	public void onCreate() {
