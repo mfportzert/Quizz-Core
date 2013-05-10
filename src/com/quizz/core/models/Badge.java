bad260418887d150184cd876e852bd7523445519
mfportzert <michelf.portzert@gmail.com>
2013-05-10 05:49:25 +0200

    Changed Badge icon variable type to int (for resource)

--- src/com/quizz/core/models/Badge.java
+++ src/com/quizz/core/models/Badge.java
@@ -1,13 +1,23 @@
 package com.quizz.core.models;
 
-public class Badge {
 
+public class Badge {
+	
 	public final static int STATUS_LOCKED = 0;
 	public final static int STATUS_UNLOCKED = 1;
 	
 	public int requiredLevelProgression;
 	public int label;
-	public String icon;
+	public int icon;
 	public int status = STATUS_UNLOCKED;
 	
+	public Badge(int label, int icon, int requiredLevel) {
+		this.requiredLevelProgression = requiredLevel;
+		this.icon = icon;
+		this.label = label;
+	}
+	
+	public Badge() {
+		
+	}
 }
